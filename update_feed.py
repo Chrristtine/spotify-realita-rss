cffb9c48f2d4ce00e7e9469a8dac9f1a736cfddb
Chrristtine <brittanymiller1994@gmail.com>
2026-09-18 23:03:40 +0200

    Update update_feed.py

--- update_feed.py
+++ update_feed.py
@@ -7,8 +7,14 @@ from xml.etree.ElementTree import Element, SubElement, ElementTree
 
 ARTIST_ID = "56DyVeJDf5P6envEFo6s7X"
 ARTIST_URL = f"https://open.spotify.com/artist/{ARTIST_ID}"
+
+# Discord role Fanoušci 🔔
+ROLE_ID = "1531371818772861089"
+ROLE_MENTION = f"<@&{ROLE_ID}>"
+
 CLIENT_ID = os.environ["SPOTIFY_CLIENT_ID"]
 CLIENT_SECRET = os.environ["SPOTIFY_CLIENT_SECRET"]
+
 
 def get_token():
     response = requests.post(
@@ -20,12 +26,14 @@ def get_token():
     response.raise_for_status()
     return response.json()["access_token"]
 
+
 def get_releases(token):
     headers = {"Authorization": f"Bearer {token}"}
     releases = []
+
     url = (
         f"https://api.spotify.com/v1/artists/{ARTIST_ID}/albums"
-        "?include_groups=album,single&market=CZ&limit=10"
+        "?include_groups=single&market=CZ&limit=10"
     )
 
     while url and len(releases) < 50:
@@ -35,10 +43,7 @@ def get_releases(token):
         releases.extend(data.get("items", []))
         url = data.get("next")
 
-    # Remove duplicate album IDs that can occur because of market/versions.
-    unique = {}
-    for item in releases:
-        unique[item["id"]] = item
+    unique = {item["id"]: item for item in releases}
 
     def release_date(item):
         parts = item.get("release_date", "1900-01-01").split("-")
@@ -49,59 +54,82 @@ def get_releases(token):
 
     return sorted(unique.values(), key=release_date, reverse=True)[:20]
 
+
 def parse_date(item):
     value = item.get("release_date", "")
     precision = item.get("release_date_precision", "day")
+
     if precision == "day":
         return datetime.strptime(value, "%Y-%m-%d").replace(tzinfo=timezone.utc)
+
     if precision == "month":
         return datetime.strptime(value, "%Y-%m").replace(tzinfo=timezone.utc)
+
     return datetime.strptime(value, "%Y").replace(tzinfo=timezone.utc)
 
+
 def make_rss(releases):
-    rss = Element("rss", {
-        "version": "2.0",
-        "xmlns:atom": "http://www.w3.org/2005/Atom",
-        "xmlns:content": "http://purl.org/rss/1.0/modules/content/",
-    })
+    rss = Element(
+        "rss",
+        {
+            "version": "2.0",
+            "xmlns:atom": "http://www.w3.org/2005/Atom",
+            "xmlns:content": "http://purl.org/rss/1.0/modules/content/",
+        },
+    )
 
     channel = SubElement(rss, "channel")
     SubElement(channel, "title").text = "Realita – nové releasy na Spotify"
     SubElement(channel, "link").text = ARTIST_URL
-    SubElement(channel, "description").text = "Nové skladby a releasy interpreta Realita na Spotify."
+    SubElement(channel, "description").text = (
+        "Nové skladby interpreta Realita na Spotify."
+    )
     SubElement(channel, "language").text = "cs-CZ"
     SubElement(channel, "ttl").text = "60"
 
     for item in releases:
         item_el = SubElement(channel, "item")
+
         title = item.get("name", "Nový release")
         spotify_url = item["external_urls"]["spotify"]
         release_date = parse_date(item)
+
         image = ""
         if item.get("images"):
             image = item["images"][0].get("url", "")
 
         SubElement(item_el, "title").text = title
         SubElement(item_el, "link").text = spotify_url
-        SubElement(item_el, "guid", {"isPermaLink": "false"}).text = f"spotify:album:{item['id']}"
+        SubElement(item_el, "guid", {"isPermaLink": "false"}).text = (
+            f"spotify:album:{item['id']}"
+        )
         SubElement(item_el, "pubDate").text = format_datetime(release_date)
 
         description = (
+            f'<p>🎵 <strong>Nová skladba od Realita!</strong></p>'
             f'<p><strong>{html.escape(title)}</strong></p>'
-            f'<p>Nový release interpreta Realita na Spotify.</p>'
-            f'<p><a href="{html.escape(spotify_url)}">Poslechnout na Spotify</a></p>'
+            f'<p>{ROLE_MENTION}</p>'
+            f'<p><a href="{html.escape(spotify_url)}">'
+            f'🎧 Poslechnout na Spotify'
+            f'</a></p>'
         )
+
         if image:
             description = (
-                f'<p><img src="{html.escape(image)}" alt="{html.escape(title)}" /></p>'
+                f'<p><img src="{html.escape(image)}" '
+                f'alt="{html.escape(title)}" /></p>'
                 + description
             )
 
         SubElement(item_el, "description").text = description
         SubElement(item_el, "content:encoded").text = description
 
-    tree = ElementTree(rss)
-    tree.write("rss.xml", encoding="utf-8", xml_declaration=True)
+    ElementTree(rss).write(
+        "rss.xml",
+        encoding="utf-8",
+        xml_declaration=True
+    )
+
 
 if __name__ == "__main__":
     token = get_token()
